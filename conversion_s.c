f5dd12d9b497b653002e5a5200feca5fe7477a2b
ThatFireBoi <gcf2007@hotmail.com>
2023-07-28 03:05:31 +0000

    Added betty style coding and binary conversion functions

--- conversion_s.c
+++ conversion_s.c
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 
 /**
+ * print_char - Conversion and printing of characters
  *
+ * @args: va_list containing the character to be printed
  *
- *
- *
- *
+ * Return: number of characters printed, 1.
  */
 
 int print_char(va_list args)
@@ -17,6 +17,14 @@ int print_char(va_list args)
 	write(1, &c, 1);
 	return (1);
 }
+
+/**
+ * print_string - Conversion and printing of strings
+ *
+ * @args: va_list containing the string to be printed
+ *
+ * Return: number of characters printed, not the NULL byte
+ */
 
 int print_string(va_list args)
 {
@@ -27,6 +35,14 @@ int print_string(va_list args)
 	return write(1, str, length);
 }
 
+/**
+ * print_integer - Conversion and printing of integers
+ *
+ * @args: va_list containing the integer to be printed
+ *
+ * Return: number of integers printed
+ */
+
 int print_integer(va_list args)
 {
 	int num = va_arg(args, int);
@@ -35,3 +51,62 @@ int print_integer(va_list args)
 	return write(1, buffer, length);
 }
 
+/**
+ * print_binary - Conversion and printing of binary numbers
+ *
+ * @args: va_list containing the integer to be printed un binary
+ *
+ * Return: number of characters printed
+ */
+
+int print_binary(va_list args)
+{
+	int num = va_arg(args, int);
+	char buffer[33];
+	int count = 0;
+	int index = 0;
+	int i;
+	int j;
+	int isNegative = 0;
+
+	if (num == 0)
+	{
+		buffer[index++] = '0';
+	}
+	else
+	{
+		if (num < 0)
+		{
+			isNegative = 1;
+			num = -num;
+		}
+
+		while (num != 0)
+		{
+			buffer[index++] = (num & 1) ? '1' : '0';
+			num >>= 1;
+		}
+
+		while (index <32)
+		{
+			buffer[index++] = '0';
+		}
+
+		for (i = isNegative, j = index -1; i < j; i++, j--)
+		{
+			char temp = buffer[i];
+			buffer[i] = buffer[j];
+			buffer[j] = temp;
+		}
+
+		if (isNegative)
+		{
+			buffer[0] = '-';
+		}
+	}
+
+	buffer[index] = '\0';
+	count += write(1, buffer, index);
+
+	return count;
+}
--- main.h
+++ main.h
@@ -5,6 +5,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * struct FormatHandler - struct that handles format specifiers and handlers
+ *
+ * @specifier: format specifier
+ *
+ * @handler: function pointer to the corresponding handler
+ */
 
 /*Prototype for _printf function, prints to formated output*/
 int _printf(const char *format, ...);
@@ -21,7 +28,10 @@ int print_string(va_list args);
 /*Integer function declaration*/
 int print_integer(va_list args);
 
-typedef struct
+/*Binary function specifier*/
+int print_binary(va_list args);
+
+typedef struct FormatHandler
 {
   char specifier;
   int (*handler)(va_list);
